459e2f340994c03c94b5c239f32770f7e6ef088a
0x5b62656e5d <dev.benjamink@gmail.com>
2025-05-30 16:21:53 +0800

    Update hello response

--- src/routes/hello.rs
+++ src/routes/hello.rs
@@ -1,6 +1,19 @@
 use actix_web::{HttpResponse, Responder, get};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HelloResponse {
+    success: bool,
+    message: String,
+}
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
+    let response = HelloResponse {
+        success: true,
+        message: "Hello, world!".to_string(),
+    };
+
+    HttpResponse::Ok().json(response)
 }
